790194a70417b80ff2a4b806bff9c29e5e3b0f7f
Himanshu Yadav <hiyada@microsoft.com>
2019-08-07 17:12:12 +0530

    Changed package.json information and rearranged code.

--- routes/queries.js
+++ routes/queries.js
@@ -7,10 +7,10 @@ router.get('/', function (req, res) {
     res.render('queries', {
         title: 'GitHub Queries',
         assignee: req.query.assignee,
+        repo: req.query.repo, //format: organization/repoName
         showOnlyStaleIssues: req.query.showOnlyStaleIssues,
         chartType: req.query.chartType,
-        showTeamDashboard: req.query.showTeamDashboard,
-        repo: req.query.repo //format: organization/repoName
+        showTeamDashboard: req.query.showTeamDashboard
     });
 });
 
